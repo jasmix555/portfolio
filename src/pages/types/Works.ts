dbc73a54fea47900239805a8f32590ab1276f655
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-11-14 09:56:21 +0900

    Refactor code to improve performance and readability.

---
+++ src/pages/types/Works.ts
@@ -0,0 +1,75 @@
+const works = [
+  {
+    title: "SpaceLang",
+    link: "team-project2023-9rrxwr8ry-jasmix555s-projects.vercel.app",
+    description: "流行してる料理や新しい料理の経験を簡単で探せるアプリ",
+    thumbnail: "",
+    tags: ["Next.Js", "SASS", "Firebase", "Figma"],
+    method: ["2年生", "学校", "チーム開発"],
+    role: ["メインエンジニア", "デザイナー"],
+    dateCreated: "2023/10 ~ 現在",
+    awards: "",
+  },
+
+  {
+    title: "Foodera",
+    link: "foodera-i4mz3d1k8-jasmix555s-projects.vercel.app",
+    description: "流行してる料理や新しい料理の経験を簡単で探せるアプリ",
+    thumbnail: "",
+    tags: ["PUG", "SASS", "JavaScript", "Illustrator", "Figma"],
+    method: ["2年生", "学校", "個人開発"],
+    role: ["メインエンジニア", "メインデザイナー"],
+    dateCreated: "2023/05 ~ 2023/06",
+    awards: "",
+  },
+
+  {
+    title: "Sakamachi (酒街)",
+    link: "oh-matchly-kvqov15on-jasmix555s-projects.vercel.app",
+    description: "数少ない深夜営業の居酒屋探しが楽になる",
+    thumbnail: "",
+    tags: ["PUG", "SASS", "JavaScript", "Illustrator", "Figma"],
+    method: ["2年生", "学校", "チーム開発"],
+    role: ["メインエンジニア", "デザイナー"],
+    dateCreated: "2023/04 ~ 2023/06",
+    awards: "",
+  },
+
+  {
+    title: "Chittle",
+    link: "https://click.ecc.ac.jp/ecc/json/Chittle/",
+    description:
+      "食品ロスを増やさないためにサイズを小さくした調味料の販売に力を入れているサイト",
+    thumbnail: "",
+    tags: ["HTML", "CSS", "JavaScript", "Illustrator", "Figma"],
+    method: ["1年生", "学校", "個人開発"],
+    role: ["メインエンジニア", "デザイナー"],
+    dateCreated: "2022/12 ~ 2023/01",
+    awards: "",
+  },
+
+  {
+    title: "Nemu-Nemu Kun LP",
+    link: "https://click.ecc.ac.jp/ecc/json/Nemu-Nemu-Kun/",
+    description:
+      "睡眠時間を確保できるように、就寝時間の管理とサポートをするアプリ",
+    thumbnail: "",
+    tags: ["HTML", "CSS", "JavaScript"],
+    method: ["1年生", "学校", "チーム開発"],
+    role: ["メインエンジニア"],
+    dateCreated: "2022/09 ~ 2022/09",
+    awards: "",
+  },
+
+  {
+    title: "+62Resto",
+    link: "https://click.ecc.ac.jp/ecc/json/+62Resto/",
+    description: "インドネシア料理テーマカフェサイト",
+    thumbnail: "",
+    tags: ["HTML", "CSS", "AdobeXD", "Illustrator"],
+    method: ["1年生", "学校", "個人開発"],
+    role: ["メインエンジニア"],
+    dateCreated: "2022/07 ~ 2022/08",
+    awards: "",
+  },
+];
