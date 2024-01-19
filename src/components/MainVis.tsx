689687ba5b8a6c526f6cf924c7a177064f527c00
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-01-19 15:49:57 +0900

    Refactor MainVis component

--- src/components/MainVis.tsx
+++ src/components/MainVis.tsx
@@ -2,32 +2,21 @@ import React from "react";
 import BoxComponent from "./Box";
 import ThreeJS from "./ThreeJS";
 import AnimatedDiv from "./AnimatedDiv";
+import style from "@/styles/MainVis.module.scss";
 
 export default function MainVis() {
   return (
     <BoxComponent heading="Top" id={"Top"}>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <div className={style.mainVis}>
         <ThreeJS />
-        <AnimatedDiv delay={0.6}>
-          <div>
-            <h1>Hi I&apos;m Jason Ng</h1>
-            <p
-              style={{
-                fontSize: "1.8rem",
-              }}
-            >
-              An Aspiring Front End Engineer
-              <br />
-              and this is my Portfolio.
-            </p>
-          </div>
-        </AnimatedDiv>
+        <div>
+          <AnimatedDiv delay={0.6}>Hi I&apos;m Jason Ng</AnimatedDiv>
+          <AnimatedDiv delay={0.7}>
+            An Aspiring Front End Engineer
+            <br />
+            and this is my Portfolio.
+          </AnimatedDiv>
+        </div>
       </div>
     </BoxComponent>
   );
