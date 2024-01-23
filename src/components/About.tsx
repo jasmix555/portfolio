d109480441961d9f1f29025278eac9b137ac36fe
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-01-23 12:25:06 +0900

    Update styles and links in Contact and Archive modules

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,9 +18,9 @@ const about = {
     {
       category: "Coding/Programming",
       items: [
-        { name: "HTML", icon: SiHtml5, duration: "2 years" },
-        { name: "SCSS", icon: SiSass, duration: "1.5 year" },
-        { name: "JavaScript", icon: SiJavascript, duration: "1 year" },
+        { name: "HTML", icon: SiHtml5, duration: "1.5 years" },
+        { name: "SCSS", icon: SiSass, duration: "1.2 year" },
+        { name: "JavaScript", icon: SiJavascript, duration: "8 months" },
         { name: "Next.js", icon: SiNextdotjs, duration: "4 months" },
       ],
     },
@@ -30,7 +30,7 @@ const about = {
       items: [
         { name: "Illustrator", icon: SiAdobeillustrator, duration: "2 years" },
         { name: "Photoshop", icon: SiAdobephotoshop, duration: "2 years" },
-        { name: "Figma", icon: SiFigma, duration: "1.5 year" },
+        { name: "Figma", icon: SiFigma, duration: "1 year" },
       ],
     },
 
@@ -48,17 +48,28 @@ function About() {
   return (
     <BoxComponent heading="About" id={"About"} height={"100vh"}>
       <div className={style.testimonialGrid}>
-        <AnimatedDiv className={style.testimonial}>
-          <h1>Jason Ng</h1>
+        <AnimatedDiv className={`${style.testimonial} ${style.gridColSpan2}`}>
+          <div className={style.title}>
+            <h1>My Info</h1>
+          </div>
+          <div>
+            <p>Name: Jason Ng</p>
+            <p>Birthdate: 2001/11/02</p>
+            <p>Nationality: Indonesian</p>
+            <p>Languages:🇺🇸 🇮🇩 🇯🇵</p>
+          </div>
         </AnimatedDiv>
 
-        <AnimatedDiv className={`${style.testimonial} ${style.image}`}>
-          <div className={style.profile}></div>
+        <AnimatedDiv className={style.testimonial}>
+          <div className={style.title}>
+            <h1>My Story</h1>
+          </div>
+          <p>Currently a student studying web design abroad in Japan</p>
         </AnimatedDiv>
 
         <AnimatedDiv className={`${style.testimonial} ${style.gridRowSpan2}`}>
           <div className={style.title}>
-            <h1>My Skills</h1>
+            <h1>Tech Stack</h1>
           </div>
           <div className={style.categoryWrapper}>
             {about.skills.map((category, idx) => (
@@ -80,15 +91,8 @@ function About() {
           </div>
         </AnimatedDiv>
 
-        <AnimatedDiv className={style.testimonial}>
-          <div className={style.title}>
-            <h1>My Info</h1>
-          </div>
-          <div>
-            <p>Birthdate: 2001/11/02</p>
-            <p>Nationality: Indonesian</p>
-            <p>Languages:🇺🇸 🇮🇩 🇯🇵</p>
-          </div>
+        <AnimatedDiv className={`${style.testimonial} ${style.image}`}>
+          <div className={style.profile}></div>
         </AnimatedDiv>
 
         <AnimatedDiv className={style.testimonial}>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,30 +7,36 @@ const contacts = [
   {
     icon: SiGmail,
     link: "mailto:Jasmix555@gmail.com",
+    text: "Send Mail",
   },
   {
     icon: SiInstagram,
     link: "https://www.instagram.com/jason_ng555/",
+    text: "Instagram Profile",
   },
   {
     icon: SiGithub,
     link: "https://github.com/jasmix555",
+    text: "GitHub Profile",
   },
   {
     icon: SiWantedly,
     link: "https://www.wantedly.com/id/jason_ng555",
+    text: "Wantedly Profile",
   },
 ];
 
 function Contact() {
   return (
-    <BoxComponent heading="Contact" bgc={"#f5f5f5"} id={"Contact"}>
+    <BoxComponent heading="Contact" id={"Contact"}>
       <div className={style.wrapper}>
         <div className={style.icons}>
           {contacts.map((e, idx) => (
-            <Link href={e.link} key={idx}>
-              <e.icon />
-            </Link>
+            <div data-name={`${e.text}`} className={style.box}>
+              <Link href={e.link} key={idx}>
+                <e.icon />
+              </Link>
+            </div>
           ))}
         </div>
       </div>
