7febbc1f42ad93946976f4291b0d6c9ee70f0680
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-01-23 12:58:45 +0900

    Add style class to Project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,6 @@
 import AnimatedDiv from "./AnimatedDiv";
 import BoxComponent from "./Box";
+import style from "@/styles/Project.module.scss";
 
 export default function Project() {
   return (
@@ -9,17 +10,7 @@ export default function Project() {
       bgc={"#f5f5f5"}
       height={"100vh"}
     >
-      <AnimatedDiv
-        style={{
-          width: "90vw",
-          height: "90vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: " 0 auto",
-          backgroundColor: "#eee",
-        }}
-      >
+      <AnimatedDiv className={style.wrapper}>
         <h1>Main Project</h1>
       </AnimatedDiv>
     </BoxComponent>
