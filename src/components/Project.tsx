2c3fa9d533b3e6c915199e5d82eb3057d8254d6a
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-01-30 10:31:32 +0900

    add carousel

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,7 @@
-import AnimatedDiv from "./AnimatedDiv";
 import BoxComponent from "./Box";
 import style from "@/styles/Project.module.scss";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function Project() {
   return (
@@ -10,9 +11,31 @@ export default function Project() {
       bgc={"#f5f5f5"}
       height={"100vh"}
     >
-      <AnimatedDiv className={style.wrapper}>
-        <h1>Main Project</h1>
-      </AnimatedDiv>
+      <div className={style.wrapper}>
+        <Carousel
+          swipeable={true}
+          infiniteLoop={true}
+          autoPlay={true} // set the autoplay interval (in milliseconds)
+          emulateTouch={true}
+        >
+          <div>
+            <img src="/projects/1.png" />
+            <p className="legend">SpaceLang</p>
+          </div>
+          <div>
+            <img src="/projects/2.png" />
+            <p className="legend">Sakamachi</p>
+          </div>
+          <div>
+            <img src="/projects/3.png" />
+            <p className="legend">Foodera</p>
+          </div>
+          <div>
+            <img src="/projects/4.png" />
+            <p className="legend">Attendance</p>
+          </div>
+        </Carousel>
+      </div>
     </BoxComponent>
   );
 }
